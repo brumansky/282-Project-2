1426e871d4ed842da8494f4a7f44e1ff59ff08ce
brumansky <brumansky@gmail.com>
2017-06-26 00:44:53 -0700

    containsKey method

--- Node.java
+++ Node.java
@@ -1,6 +1,37 @@
 
-public class Node {
-    private Node right;
-    private Node left;
+public class Node<K extends Comparable, V>{
+    private K key;
+    private V value;
+    private Node<K , V> right;
+    private Node<K , V> left;
     private boolean isRed;
+
+    Node (K keyarg, V valuearg) {
+        key = keyarg;
+        value = valuearg;
+        left = null;
+        right = null;
+        isRed = true;
+
+    }
+
+    public boolean containsKey(K keyarg) {
+
+        Node tmp = this;
+        int difference;
+        
+        while (tmp != null) {
+            difference = keyarg.compareTo(tmp.key);
+            if (difference == 0)
+                return true;
+            else if (difference > 0) {
+                tmp = tmp.right;
+            } else {
+                tmp = tmp.left;
+            }
+
+        }
+        return false;
+
+    }
 }
